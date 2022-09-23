6c5bba5262fb0fdc8290f650c0d42e7cab36bafd
gowthamsubburaj23ece <113689813+gowthamsubburaj23ece@users.noreply.github.com>
2022-09-23 16:37:36 +0530

    initialcommit
    initialcommit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "gowthams";
+$username = "gowthams";
+$password = "Linux@123";
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
